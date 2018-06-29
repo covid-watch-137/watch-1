bcf32ad9249858d0ab4c0015480a2a7c39819a5b
Jordan Price <jprice@izeni.com>
2018-06-29 15:15:32 -0600

    Queryset filtering for facilities.  Adjust queryset filtering for Organizations

--- apps/core/api.py
+++ apps/core/api.py
@@ -3,6 +3,7 @@ from rest_framework import serializers, viewsets, permissions, mixins
 from apps.core.models import (
     Organization, Facility, ProviderProfile, ProviderTitle, ProviderRole,
     ProviderSpecialty, Diagnosis)
+from apps.patients.models import PatientProfile
 
 
 class OrganizationSerializer(serializers.ModelSerializer):
@@ -20,19 +21,28 @@ class OrganizationViewSet(
     permission_classes = (permissions.IsAuthenticated, )
 
     def get_queryset(self):
-        qs = Organization.objects.none()
-        provider_profile = self.request.user.provider_profile
+        qs = Organization.objects.all()
+        # if self.request.user.is_superuser:
+        #     return qs.all()
+        try:
+            provider_profile = self.request.user.provider_profile
+        except ProviderProfile.DoesNotExist:
+            provider_profile = None
         if provider_profile is not None:
-            qs = Organization.objects.filter(
+            qs = qs.filter(
                 Q(id__in=provider_profile.organizations.all()) |
                 Q(id__in=provider_profile.organizations_managed.all())
             )
-            return qs
-        patient_profile = self.request.user.patient_profile
+            return qs.all()
+        try:
+            patient_profile = self.request.user.patient_profile
+        except PatientProfile.DoesNotExist:
+            patient_profile = None
         if patient_profile is not None:
-            qs = Organization.objects.filter(
+            qs = qs.filter(
                 id=patient_profile.facility.organization.id)
-        return qs
+            return qs.all()
+        return qs.none()
 
 
 class FacilitySerializer(serializers.ModelSerializer):
@@ -44,7 +54,30 @@ class FacilitySerializer(serializers.ModelSerializer):
 class FacilityViewSet(viewsets.ModelViewSet):
     serializer_class = FacilitySerializer
     permission_classes = (permissions.IsAuthenticated, )
-    queryset = Facility.objects.all()
+
+    def get_queryset(self):
+        # QUESTION: should providers only have relations to facilities,
+        # and not organizations?
+        qs = Facility.objects.all()
+        # if self.request.user.is_superuser:
+        #     return qs.all()
+        try:
+            provider_profile = self.request.user.provider_profile
+        except ProviderProfile.DoesNotExist:
+            provider_profile = None
+        if provider_profile is not None:
+            qs = qs.filter(
+                Q(id__in=provider_profile.facilities.all()) |
+                Q(id__in=provider_profile.facilities_managed.all())
+            )
+            return qs.all()
+        try:
+            patient_profile = self.request.user.patient_profile
+        except PatientProfile.DoesNotExist:
+            patient_profile = None
+        if patient_profile is not None:
+            qs = qs.filter(id=patient_profile.facility.id)
+            return qs.all()
 
 
 class ProviderProfileSerializer(serializers.ModelSerializer):
@@ -57,6 +90,8 @@ class ProviderProfileViewSet(viewsets.ModelViewSet):
     serializer_class = ProviderProfileSerializer
     permission_classes = (permissions.IsAuthenticated, )
     queryset = ProviderProfile.objects.all()
+    # TODO: For providers, only return providers in the same facilities/organizations
+    # TODO: For patients, only return providers that are providers for the patient
 
 
 class ProviderTitleSerializer(serializers.ModelSerializer):
@@ -67,7 +102,7 @@ class ProviderTitleSerializer(serializers.ModelSerializer):
 
 class ProviderTitleViewSet(viewsets.ModelViewSet):
     serializer_class = ProviderTitleSerializer
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = (permissions.AllowAny, )
     queryset = ProviderTitle.objects.all()
 
 
@@ -79,7 +114,7 @@ class ProviderRoleSerializer(serializers.ModelSerializer):
 
 class ProviderRoleViewSet(viewsets.ModelViewSet):
     serializer_class = ProviderRoleSerializer
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = (permissions.AllowAny, )
     queryset = ProviderRole.objects.all()
 
 
@@ -91,7 +126,7 @@ class ProviderSpecialtySerializer(serializers.ModelSerializer):
 
 class ProviderSpecialtyViewSet(viewsets.ModelViewSet):
     serializer_class = ProviderSpecialtySerializer
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = (permissions.AllowAny, )
     queryset = ProviderSpecialty.objects.all()
 
 
@@ -103,5 +138,5 @@ class DiagnosisSerializer(serializers.ModelSerializer):
 
 class DiagnosisViewSet(viewsets.ModelViewSet):
     serializer_class = DiagnosisSerializer
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = (permissions.AllowAny, )
     queryset = Diagnosis.objects.all()
