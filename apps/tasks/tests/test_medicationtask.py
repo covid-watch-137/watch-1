96e9014a55c53f385ce4c4c4fb39115a53f1a8d5
Ronil Rufo <ronil.rufo@gmail.com>
2018-09-14 14:16:32 +0800

    #24: Added test for patient and medication task for unauthenticated requests

--- apps/tasks/tests/test_medicationtask.py
+++ apps/tasks/tests/test_medicationtask.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 
 from faker import Faker
+from rest_framework import status
 from rest_framework.test import APITestCase
 
 from .mixins import TasksMixin
@@ -29,3 +30,12 @@ class TestMedicationTask(TasksMixin, APITestCase):
         }
         response = self.client.patch(self.detail_url, payload)
         self.assertEqual(response.data['status'], done)
+
+    def test_update_status_medication_task_unauthenticated(self):
+        self.client.logout()
+        done = 'done'
+        payload = {
+            'status': done
+        }
+        response = self.client.patch(self.detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
