7c81672238db2a2135c502f4b03c6ea2713af533
Ronil Rufo <ronil.rufo@gmail.com>
2018-09-15 16:27:22 +0800

    #27: Added test cases for permission checks in PatientTaskViewSet using a patient

--- apps/tasks/tests/test_patienttask.py
+++ apps/tasks/tests/test_patienttask.py
@@ -218,3 +218,148 @@ class TestPatientTaskUsingEmployee(TasksMixin, APITestCase):
         url = reverse('patient_tasks-detail', kwargs={'pk': patient_task.id})
         response = self.client.delete(url, {})
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class TestPatientTaskUsingPatient(TasksMixin, APITestCase):
+    """
+    Test cases for :model:`tasks.PatientTask` using a patient
+    as the logged in user.
+    """
+
+    def setUp(self):
+        self.fake = Faker()
+        self.patient = self.create_patient()
+        self.user = self.patient.user
+
+        self.plan = self.create_care_plan(patient=self.patient)
+        self.patient_task = self.create_patient_task(**{
+            'plan': self.plan
+        })
+        self.url = reverse('patient_tasks-list')
+        self.detail_url = reverse(
+            'patient_tasks-detail',
+            kwargs={'pk': self.patient_task.id}
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_patient_tasks_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.data['count'], 1)
+
+    def test_get_patient_task_detail(self):
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_patient_task_detail_unauthenticated(self):
+        self.client.logout()
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_patient_task_detail_not_owner(self):
+        task = self.create_patient_task()
+        url = reverse('patient_tasks-detail', kwargs={'pk': task.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_create_patient_task(self):
+        template = self.create_patient_task_template()
+
+        appear_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+5d")
+        )
+
+        due_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+30d")
+        )
+
+        payload = {
+            'plan': self.plan.id,
+            'patient_task_template': template.id,
+            'appear_datetime': appear_datetime,
+            'due_datetime': due_datetime,
+        }
+        response = self.client.post(self.url, payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_full_update_patient_task(self):
+        template = self.create_patient_task_template()
+
+        appear_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+5d")
+        )
+
+        due_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+30d")
+        )
+        payload = {
+            'plan': self.plan.id,
+            'patient_task_template': template.id,
+            'appear_datetime': appear_datetime,
+            'due_datetime': due_datetime,
+        }
+        response = self.client.put(self.detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_full_update_patient_task_not_owner(self):
+        template = self.create_patient_task_template()
+
+        appear_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+5d")
+        )
+
+        due_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+30d")
+        )
+        payload = {
+            'plan': self.plan.id,
+            'patient_task_template': template.id,
+            'appear_datetime': appear_datetime,
+            'due_datetime': due_datetime,
+        }
+
+        patient_task = self.create_patient_task()
+        url = reverse('patient_tasks-detail', kwargs={'pk': patient_task.id})
+        response = self.client.put(url, payload)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_partial_update_patient_task(self):
+        appear_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+5d")
+        )
+
+        due_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+30d")
+        )
+        payload = {
+            'appear_datetime': appear_datetime,
+            'due_datetime': due_datetime,
+        }
+        response = self.client.patch(self.detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_partial_update_patient_task_not_owner(self):
+        appear_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+5d")
+        )
+
+        due_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+30d")
+        )
+        payload = {
+            'appear_datetime': appear_datetime,
+            'due_datetime': due_datetime,
+        }
+        patient_task = self.create_patient_task()
+        url = reverse('patient_tasks-detail', kwargs={'pk': patient_task.id})
+        response = self.client.patch(url, payload)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_patient_task(self):
+        response = self.client.delete(self.detail_url, {})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_patient_task_not_member(self):
+        patient_task = self.create_patient_task()
+        url = reverse('patient_tasks-detail', kwargs={'pk': patient_task.id})
+        response = self.client.delete(url, {})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
