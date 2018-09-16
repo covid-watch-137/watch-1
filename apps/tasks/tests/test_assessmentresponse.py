52aae04380a4d393bea1d7e0d064eb9600568a35
Ronil Rufo <ronil.rufo@gmail.com>
2018-09-16 09:35:30 +0800

    #27: Implemented test case for filtering in AssessmentResponseViewSet

--- apps/tasks/tests/test_assessmentresponse.py
+++ apps/tasks/tests/test_assessmentresponse.py
@@ -7,6 +7,47 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from .mixins import TasksMixin
+from apps.accounts.tests.factories import AdminUserFactory
+
+
+class TestAssessmentResponse(TasksMixin, APITestCase):
+    """
+    Test cases for :model:`tasks.AssessmentResponse`
+    """
+
+    def setUp(self):
+        self.fake = Faker()
+        self.user = AdminUserFactory()
+
+        self.plan = self.create_care_plan()
+        self.assessment_task = self.create_assessment_task(**{
+            'plan': self.plan
+        })
+        self.template = self.assessment_task.assessment_task_template
+        self.create_multiple_assessment_questions(self.template)
+        self.create_responses_to_multiple_questions(
+            self.template,
+            self.assessment_task,
+            self.template.assessmentquestion_set.all()
+        )
+        self.responses = self.assessment_task.assessmentresponse_set.all()
+        self.assessment_response = random.choice(self.responses)
+        self.url = reverse('assessment_responses-list')
+        self.detail_url = reverse(
+            'assessment_responses-detail',
+            kwargs={'pk': self.assessment_response.id}
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_filter_by_assessment_task_template(self):
+        filter_url = f'{self.url}?assessment_task__assessment_task_template__id={self.template.id}'
+        response = self.client.get(filter_url)
+        self.assertEqual(response.data['count'], self.responses.count())
+
+    def test_filter_by_assessment_question(self):
+        filter_url = f'{self.url}?assessment_question__id={self.assessment_response.assessment_question.id}'
+        response = self.client.get(filter_url)
+        self.assertEqual(response.data['count'], 1)
 
 
 class TestAssessmentResponseUsingEmployee(TasksMixin, APITestCase):
