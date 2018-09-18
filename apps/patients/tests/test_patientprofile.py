ed91ea46aac9fc9b7d8bcf89d6b83e70f7ab23bd
Ronil Rufo <ronil.rufo@gmail.com>
2018-09-18 09:38:28 +0800

    #17: Fixed typos and implemented test case for task percentage

---
+++ apps/patients/tests/test_patientprofile.py
@@ -0,0 +1,70 @@
+from django.urls import reverse
+
+from faker import Faker
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from apps.tasks.tests.mixins import TasksMixin
+
+
+class TestPatientProfile(TasksMixin, APITestCase):
+    """
+    Test cases for :model:`tasks.PatientTask` using a patient
+    as the logged in user.
+    """
+
+    def setUp(self):
+        self.fake = Faker()
+        self.patient = self.create_patient()
+        self.user = self.patient.user
+
+        self.plan = self.create_care_plan(patient=self.patient)
+        self.dashboard_url = reverse('patient_profiles-patient-dashboard')
+        self.client.force_authenticate(user=self.user)
+
+    def generate_patient_tasks(self):
+        for i in range(3):
+            # Create PatientTask with status `undefined`
+            self.create_patient_task(**{'plan': self.plan})
+
+            # Create PatientTask with status `done`
+            self.create_patient_task(**{
+                'plan': self.plan,
+                'status': 'done'
+            })
+
+    def generate_medication_tasks(self):
+        template = self.create_medication_task_template(plan=self.plan)
+        for i in range(3):
+            # Create MedicationTask with status `undefined`
+            self.create_medication_task(**{
+                'medication_task_template': template
+            })
+
+            # Create MedicationTask with status `done`
+            self.create_medication_task(**{
+                'medication_task_template': template,
+                'status': 'done'
+            })
+
+    def generate_symptom_tasks(self):
+        for i in range(5):
+            self.create_symptom_task(**{
+                'plan': self.plan
+            })
+
+            if i < 3:
+                task = self.create_symptom_task(**{
+                    'plan': self.plan
+                })
+                self.create_symptom_rating(symptom_task=task)
+
+    def test_get_patient_dashboard_task_percentage(self):
+        self.generate_patient_tasks()
+        self.generate_medication_tasks()
+        self.generate_symptom_tasks()
+
+        response = self.client.get(self.dashboard_url)
+        patient = response.data['results'][0]
+
+        self.assertEqual(patient['task_percentage'], 45)
