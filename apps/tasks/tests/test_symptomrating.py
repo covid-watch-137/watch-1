d16f2251289d595f8be9066f34607d93f096bdf6
Ronil Rufo <ronil.rufo@gmail.com>
2018-09-19 11:34:31 +0800

    #34: Added test case for SymptomRatingViewSet using patient

--- apps/tasks/tests/test_symptomrating.py
+++ apps/tasks/tests/test_symptomrating.py
@@ -89,13 +89,137 @@ class TestSymptomRatingUsingEmployee(TasksMixin, APITestCase):
         response = self.client.put(self.detail_url, payload)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_partial_update_symptom_task(self):
+    def test_partial_update_symptom_rating(self):
         payload = {
             'rating': random.randint(1, 5),
         }
         response = self.client.patch(self.detail_url, payload)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_delete_symptom_task(self):
+    def test_delete_symptom_rating(self):
         response = self.client.delete(self.detail_url, {})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class TestSymptomTaskUsingPatient(TasksMixin, APITestCase):
+    """
+    Test cases for :model:`tasks.SymptomTask` using a patient
+    as the logged in user.
+    """
+
+    def setUp(self):
+        self.fake = Faker()
+        self.patient = self.create_patient()
+        self.user = self.patient.user
+
+        self.plan = self.create_care_plan(self.patient)
+        self.symptom_task = self.create_symptom_task(**{
+            'plan': self.plan
+        })
+        self.symptom_rating = self.create_symptom_rating(self.symptom_task)
+
+        # create other symptom rating not belonging to the employee
+        self.other_rating = self.create_symptom_rating()
+
+        self.url = reverse('symptom_ratings-list')
+        self.detail_url = reverse(
+            'symptom_ratings-detail',
+            kwargs={'pk': self.symptom_rating.id}
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_symptom_tasks_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.data['count'], 1)
+
+    def test_get_symptom_task_detail(self):
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_symptom_task_detail_unauthenticated(self):
+        self.client.logout()
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_symptom_task_detail_not_owner(self):
+        task = self.create_symptom_task()
+        url = reverse('symptom_tasks-detail', kwargs={'pk': task.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_create_symptom_rating(self):
+        task = self.create_symptom_task(**{
+            'plan': self.plan
+        })
+        symptom = self.create_symptom()
+
+        payload = {
+            'symptom_task': task.id,
+            'symptom': symptom.id,
+            'rating': random.randint(1, 5),
+        }
+        response = self.client.post(self.url, payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_full_update_symptom_rating(self):
+        task = self.create_symptom_task(**{
+            'plan': self.plan
+        })
+        symptom = self.create_symptom()
+
+        payload = {
+            'symptom_task': task.id,
+            'symptom': symptom.id,
+            'rating': random.randint(1, 5),
+        }
+        response = self.client.put(self.detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_full_update_symptom_task_not_owner(self):
+        task = self.create_symptom_task(**{
+            'plan': self.plan
+        })
+        symptom = self.create_symptom()
+
+        payload = {
+            'symptom_task': task.id,
+            'symptom': symptom.id,
+            'rating': random.randint(1, 5),
+        }
+
+        url = reverse(
+            'symptom_ratings-detail',
+            kwargs={'pk': self.other_rating.id}
+        )
+        response = self.client.put(url, payload)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_partial_update_symptom_rating(self):
+        payload = {
+            'rating': random.randint(1, 5),
+        }
+        response = self.client.patch(self.detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_partial_update_symptom_rating_not_owner(self):
+        payload = {
+            'rating': random.randint(1, 5),
+        }
+        url = reverse(
+            'symptom_ratings-detail',
+            kwargs={'pk': self.other_rating.id}
+        )
+        response = self.client.patch(url, payload)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_symptom_rating(self):
+        response = self.client.delete(self.detail_url, {})
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_symptom_rating_not_member(self):
+        url = reverse(
+            'symptom_ratings-detail',
+            kwargs={'pk': self.other_rating.id}
+        )
+        response = self.client.delete(url, {})
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
