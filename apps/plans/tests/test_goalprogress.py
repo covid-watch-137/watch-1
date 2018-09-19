8981eef8d62e8f93324922213aaceeeba1bed162
kentw832 <kentwilson832@gmail.com>
2018-09-19 16:51:58 +0800

    #16: Implemented tests for GoalProgressViewSet using patient

--- apps/plans/tests/test_goalprogress.py
+++ apps/plans/tests/test_goalprogress.py
@@ -124,3 +124,117 @@ class TestGoalProgressUsingEmployee(PlansMixin, APITestCase):
         )
         response = self.client.delete(url, {})
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class TestGoalProgressUsingPatient(PlansMixin, APITestCase):
+    """
+    Test cases for :model:`plans.GoalProgress` using a patient
+    as the logged in user.
+    """
+
+    def setUp(self):
+        self.fake = Faker()
+        self.patient = self.create_patient()
+        self.user = self.patient.user
+
+        self.plan = self.create_care_plan(self.patient)
+        self.goal = self.create_goal(**{
+            'plan': self.plan
+        })
+        self.goal_progress = self.create_goal_progress(**{
+            'goal': self.goal
+        })
+
+        # Create goal progress not belonging to the employee
+        self.other_progress = self.create_goal_progress()
+
+        self.url = reverse('goal_progresses-list')
+        self.detail_url = reverse(
+            'goal_progresses-detail',
+            kwargs={'pk': self.goal_progress.id}
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_goal_progresses_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.data['count'], 1)
+
+    def test_get_goal_progress_detail(self):
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_goal_progress_detail_unauthenticated(self):
+        self.client.logout()
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_goal_progress_detail_not_owner(self):
+        goal_progress = self.create_goal_progress()
+        url = reverse(
+            'goal_progresses-detail',
+            kwargs={'pk': goal_progress.id}
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_create_goal_progress(self):
+        payload = {
+            'goal': self.goal.id,
+            'rating': random.randint(1, 5),
+        }
+        response = self.client.post(self.url, payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_full_update_goal_progress(self):
+        payload = {
+            'goal': self.goal.id,
+            'rating': random.randint(1, 5),
+        }
+        response = self.client.put(self.detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_full_update_goal_progress_not_owner(self):
+        payload = {
+            'goal': self.goal.id,
+            'rating': random.randint(1, 5),
+        }
+
+        goal_progress = self.create_goal_progress()
+        url = reverse(
+            'goal_progresses-detail',
+            kwargs={'pk': goal_progress.id}
+        )
+        response = self.client.put(url, payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_partial_update_goal_progress(self):
+        payload = {
+            'rating': random.randint(1, 5),
+        }
+        response = self.client.patch(self.detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_partial_update_goal_progress_not_owner(self):
+        payload = {
+            'rating': random.randint(1, 5),
+        }
+        goal_progress = self.create_goal_progress()
+        url = reverse(
+            'goal_progresses-detail',
+            kwargs={'pk': goal_progress.id}
+        )
+        response = self.client.patch(url, payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_goal_progress(self):
+        response = self.client.delete(self.detail_url, {})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_goal_progress_not_member(self):
+        goal_progress = self.create_goal_progress()
+        url = reverse(
+            'goal_progresses-detail',
+            kwargs={'pk': goal_progress.id}
+        )
+        response = self.client.delete(url, {})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
