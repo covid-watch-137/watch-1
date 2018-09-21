aa6051aab419c0b60663a70369d084003523bca5
kentw832 <kentwilson832@gmail.com>
2018-09-21 10:38:55 +0800

    #16: Added tests for VitalTaskViewSet using employee

---
+++ apps/tasks/tests/test_vitaltask.py
@@ -0,0 +1,167 @@
+import pytz
+
+from django.urls import reverse
+
+from faker import Faker
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from .mixins import TasksMixin
+
+
+class TestVitalTaskUsingEmployee(TasksMixin, APITestCase):
+    """
+    Test cases for :model:`tasks.VitalTask` using an employee
+    as the logged in user.
+    """
+
+    def setUp(self):
+        self.fake = Faker()
+        self.employee = self.create_employee()
+        self.user = self.employee.user
+
+        self.plan = self.create_care_plan()
+        self.create_care_team_member(**{
+            'employee_profile': self.employee,
+            'plan': self.plan
+        })
+        self.vital_task = self.create_vital_task(**{
+            'plan': self.plan
+        })
+        self.url = reverse('vital_tasks-list')
+        self.detail_url = reverse(
+            'vital_tasks-detail',
+            kwargs={'pk': self.vital_task.id}
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_vital_tasks_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.data['count'], 1)
+
+    def test_get_vital_task_detail(self):
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_vital_task_detail_unauthenticated(self):
+        self.client.logout()
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_vital_task_detail_not_member(self):
+        task = self.create_vital_task()
+        url = reverse('vital_tasks-detail', kwargs={'pk': task.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_create_vital_task(self):
+        template = self.create_vital_task_template()
+
+        appear_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+5d")
+        )
+
+        due_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+30d")
+        )
+
+        payload = {
+            'plan': self.plan.id,
+            'vital_task_template': template.id,
+            'appear_datetime': appear_datetime,
+            'due_datetime': due_datetime,
+        }
+        response = self.client.post(self.url, payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_full_update_vital_task(self):
+        template = self.create_vital_task_template()
+
+        appear_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+5d")
+        )
+
+        due_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+30d")
+        )
+        payload = {
+            'plan': self.plan.id,
+            'vital_task_template': template.id,
+            'appear_datetime': appear_datetime,
+            'due_datetime': due_datetime,
+        }
+        response = self.client.put(self.detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_full_update_vital_task_not_member(self):
+        template = self.create_vital_task_template()
+
+        appear_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+5d")
+        )
+
+        due_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+30d")
+        )
+        payload = {
+            'plan': self.plan.id,
+            'vital_task_template': template.id,
+            'appear_datetime': appear_datetime,
+            'due_datetime': due_datetime,
+        }
+
+        vital_task = self.create_vital_task()
+        url = reverse(
+            'vital_tasks-detail',
+            kwargs={'pk': vital_task.id}
+        )
+        response = self.client.put(url, payload)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_partial_update_vital_task(self):
+        appear_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+5d")
+        )
+
+        due_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+30d")
+        )
+        payload = {
+            'appear_datetime': appear_datetime,
+            'due_datetime': due_datetime,
+        }
+        response = self.client.patch(self.detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_partial_update_vital_task_not_member(self):
+        appear_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+5d")
+        )
+
+        due_datetime = pytz.utc.localize(
+            self.fake.future_datetime(end_date="+30d")
+        )
+        payload = {
+            'appear_datetime': appear_datetime,
+            'due_datetime': due_datetime,
+        }
+        vital_task = self.create_vital_task()
+        url = reverse(
+            'vital_tasks-detail',
+            kwargs={'pk': vital_task.id}
+        )
+        response = self.client.patch(url, payload)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_vital_task(self):
+        response = self.client.delete(self.detail_url, {})
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_vital_task_not_member(self):
+        vital_task = self.create_vital_task()
+        url = reverse(
+            'vital_tasks-detail',
+            kwargs={'pk': vital_task.id}
+        )
+        response = self.client.delete(url, {})
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
