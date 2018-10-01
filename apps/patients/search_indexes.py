57a3ccef8b55bfa0f7a49f0b9a83e08b0ae9490a
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2018-10-01 23:44:34 +0800

    Added initial config for searching PatientProfile via django-haystack

---
+++ apps/patients/search_indexes.py
@@ -0,0 +1,17 @@
+from haystack import indexes
+
+from .models import PatientProfile
+
+
+class PatientProfileIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    user = indexes.CharField(model_attr='user')
+    facility = indexes.CharField(model_attr='facility')
+    emr_code = indexes.CharField(model_attr='emr_code')
+    status = indexes.CharField(model_attr='status')
+
+    def get_model(self):
+        return PatientProfile
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
