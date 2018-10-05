0bc97c6e38571a7e9511438b981968fc888d8526
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2018-10-05 12:16:22 +0800

    Fix circular import
    https://dev.izeni.net/care-adopt/care-adopt-backend/issues/60

--- apps/core/signals.py
+++ apps/core/signals.py
@@ -1,6 +1,3 @@
-from .models import InvitedEmailTemplate
-
-
 def invited_email_template_post_save(sender, instance, created, **kwargs):
     if created:
-        InvitedEmailTemplate.objects.exclude(id=instance.id).update(is_default=False)
+        sender.objects.exclude(id=instance.id).update(is_default=False)
