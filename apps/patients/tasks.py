521010cb095bead353719ea4fd1faa843f54e1ca
Ronil Rufo <ronil.rufo@gmail.com>
2018-10-06 14:05:13 +0800

    #13: Added periodic task to assign daily info messages to patients

--- apps/patients/tasks.py
+++ apps/patients/tasks.py
@@ -1,12 +1,19 @@
+import random
+
 from datetime import timedelta
 
 from celery import shared_task
+from celery.schedules import crontab
+from celery.task import PeriodicTask
 
 from apps.core.models import InvitedEmailTemplate
+from apps.plans.models import InfoMessage
+from care_adopt_backend.logger import logger
 
 from .models import PatientProfile, ReminderEmail
 
 DAYS_PASSED_BEFORE_SENDING_INVITE_REMINDER = 7
+
 
 @shared_task
 def remind_invited_patients():
@@ -28,3 +35,30 @@ def remind_invited_patients():
                 subject=reminder_template.subject,
                 message=reminder_template.message,
             )
+
+
+class DailyInfoMessage(PeriodicTask):
+    """
+    This periodic task will set an info message object into the
+    `message_for_day` field of all :model:`patients.PatientProfile`
+    on a daily basis. The info messages will be dependent on the
+    patients' care plan templates.
+    """
+
+    # Runs every midnight
+    run_every = crontab(hour='0')
+
+    def run(self):
+        logger.info("=== Running DailyInfoMessage Periodic Task  ===")
+        print("=== Running DailyInfoMessage Periodic Task  ===")
+
+        for patient in PatientProfile.objects.all():
+
+            info_messages = InfoMessage.objects.filter(
+                queue__plan_template__care_plans__patient=patient
+            )
+            msgs = list(info_messages)
+            if len(msgs) > 0:
+                msg_for_day = random.choice(msgs)
+                patient.message_for_day = msg_for_day
+                patient.save(update_fields=['message_for_day'])
