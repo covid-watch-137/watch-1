d73ae4ba3bc435e9aa928a7621da7578cc634f29
Ronil Rufo <ronil.rufo@gmail.com>
2018-10-07 08:53:16 +0800

    #64: Modified queryset and documentation in InfoMessageQueueViewSet and InfoMessageViewSet

--- apps/plans/api/views.py
+++ apps/plans/api/views.py
@@ -370,30 +370,25 @@ class InfoMessageQueueViewSet(viewsets.ModelViewSet):
 
     update:
         Updates :model:`plans.InfoMessageQueue` object.
-        Only admins and employees who belong to the same care team are allowed
-        to perform this action.
+        Only admins and employees are allowed to perform this action.
 
     partial_update:
         Updates one or more fields of an existing message queue object.
-        Only admins and employees who belong to the same care team are allowed
-        to perform this action.
+        Only admins and employees are allowed to perform this action.
 
     retrieve:
         Retrieves a :model:`plans.InfoMessageQueue` instance.
-        Admins will have access to all message queue objects. Employees will
-        only have access to those queues belonging to its own care team.
-        Patients will have access to all queues assigned to them.
+        Admins and employees will have access to all message queue objects.
+        Patients will have access to all queues in their care plans.
 
     list:
         Returns list of all :model:`plans.InfoMessageQueue` objects.
-        Admins will get all existing message queue objects. Employees will get
-        the message queue belonging to a certain care team. Patients will get
-        all queues belonging to them.
+        Admins and employees will get all existing message queue objects.
+        Patients will get all queues in their care plans.
 
     delete:
         Deletes a :model:`plans.InfoMessageQueue` instance.
-        Only admins and employees who belong to the same care team are allowed
-        to perform this action.
+        Only admins and employees are allowed to perform this action.
     """
     serializer_class = InfoMessageQueueSerializer
     permission_classes = (
@@ -406,11 +401,7 @@ class InfoMessageQueueViewSet(viewsets.ModelViewSet):
         queryset = super(InfoMessageQueueViewSet, self).get_queryset()
         user = self.request.user
 
-        if user.is_employee:
-            queryset = queryset.filter(
-                plan_template__care_plans__care_team_members__employee_profile=user.employee_profile
-            )
-        elif user.is_patient:
+        if user.is_patient:
             queryset = queryset.filter(
                 plan_template__care_plans__patient=user.patient_profile
             )
@@ -429,30 +420,25 @@ class InfoMessageViewSet(viewsets.ModelViewSet):
 
     update:
         Updates :model:`plans.InfoMessage` object.
-        Only admins and employees who belong to the same care team are allowed
-        to perform this action.
+        Only admins and employees are allowed to perform this action.
 
     partial_update:
         Updates one or more fields of an existing message object.
-        Only admins and employees who belong to the same care team are allowed
-        to perform this action.
+        Only admins and employees are allowed to perform this action.
 
     retrieve:
         Retrieves a :model:`plans.InfoMessage` instance.
-        Admins will have access to all message objects. Employees will
-        only have access to those messages belonging to its own care team.
-        Patients will have access to all messages assigned to them.
+        Admins and employees will have access to all message objects.
+        Patients will have access to all messages in their care plans.
 
     list:
         Returns list of all :model:`plans.InfoMessage` objects.
-        Admins will get all existing message objects. Employees will get
-        the message belonging to a certain care team. Patients will get
-        all messages belonging to them.
+        Admins and employees will have access to all message objects.
+        Patients will have access to all messages in their care plans.
 
     delete:
         Deletes a :model:`plans.InfoMessage` instance.
-        Only admins and employees who belong to the same care team are allowed
-        to perform this action.
+        Only admins and employees are allowed to perform this action.
     """
     serializer_class = InfoMessageSerializer
     permission_classes = (
@@ -466,10 +452,6 @@ class InfoMessageViewSet(viewsets.ModelViewSet):
         user = self.request.user
 
         if user.is_patient:
-            queryset = queryset.filter(
-                queue__plan_template__care_plans__care_team_members__employee_profile=user.employee_profile
-            )
-        elif user.is_patient:
             queryset = queryset.filter(
                 queue__plan_template__care_plans__patient=user.patient_profile
             )
