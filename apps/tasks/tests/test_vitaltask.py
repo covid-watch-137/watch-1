6a4f69c2aa998f9f73f79d3944fb47c4f8aad490
Ronil Rufo <ronil.rufo@gmail.com>
2018-10-07 10:20:48 +0800

    #62: Added tests for serializing VitalTaskTemplate onto VitalTask

--- apps/tasks/tests/test_vitaltask.py
+++ apps/tasks/tests/test_vitaltask.py
@@ -44,6 +44,13 @@ class TestVitalTaskUsingEmployee(TasksMixin, APITestCase):
         response = self.client.get(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_get_vital_task_detail_with_template_detail(self):
+        response = self.client.get(self.detail_url)
+        self.assertEqual(
+            response.data['vital_task_template']['name'],
+            self.template.name
+        )
+
     def test_get_vital_task_detail_unauthenticated(self):
         self.client.logout()
         response = self.client.get(self.detail_url)
