71620d0e258835ede8c87de859579a80dae488aa
Ronil Rufo <rrufo@mindfiretechnology.com>
2018-12-22 13:20:00 +0800

    CA-1189: Implemented viewset for retrieving goals per car plan template and patient

--- Source/Services/apps/plans/api/urls.py
+++ Source/Services/apps/plans/api/urls.py
@@ -10,6 +10,7 @@ from .views import (
 
     # GoalTemplate
     GoalTemplatesByPlanTemplate,
+    GoalsByCarePlanTemplate,
 
 )
 
@@ -35,5 +36,10 @@ urlpatterns = [
         r'^organizations/(?P<parent_lookup_care_plans__patient__facility__organization>[0-9a-f-]+)/service_areas/(?P<pk>[0-9a-f-]+)/templates/$',
         CarePlanTemplateByServiceArea.as_view(),
         name='service-area-plan-templates'
+    ),
+    url(
+        r'^patient_profiles/(?P<parent_lookup_plan__patient>[0-9a-f-]+)/care_plan_templates/(?P<pk>[0-9a-f-]+)/goals/$',
+        GoalsByCarePlanTemplate.as_view(),
+        name='goal-by-plan-templates'
     ),
 ]
