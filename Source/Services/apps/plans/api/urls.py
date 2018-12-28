d7a6e9953d6890335f1c358beb1e270727c08409
Ronil Rufo <rrufo@mindfiretechnology.com>
2018-12-28 14:12:56 +0800

    CA-1097: Implemented viewset to handle retrieval of care plans based on care plan template and facility

--- Source/Services/apps/plans/api/urls.py
+++ Source/Services/apps/plans/api/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import url
 from .views import (
 
     CarePlanTemplateByType,
+    CarePlanByTemplateFacility,
 
     # GoalTemplate
     GoalTemplatesByPlanTemplate,
@@ -22,6 +23,11 @@ urlpatterns = [
     url(
         r'^organizations/(?P<parent_lookup_care_plans__patient__facility__organization>[0-9a-f-]+)/care_plan_template_types/(?P<pk>[0-9a-f-]+)/templates/$',
         CarePlanTemplateByType.as_view(),
+        name='type-plan-templates'
+    ),
+    url(
+        r'^facilities/(?P<parent_lookup_care_plans__patient__facility>[0-9a-f-]+)/care_plan_templates/(?P<pk>[0-9a-f-]+)/care_plans/$',
+        CarePlanByTemplateFacility.as_view(),
         name='type-plan-templates'
     ),
 ]
