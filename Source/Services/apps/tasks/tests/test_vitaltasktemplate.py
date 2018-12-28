b79a28d07840b4fc904834b7d6cca0a6723bb3d8
Ronil Rufo <rrufo@mindfiretechnology.com>
2018-12-28 11:27:40 +0800

    CA-1115: Added tests for searching vital task templates

--- Source/Services/apps/tasks/tests/test_vitaltasktemplate.py
+++ Source/Services/apps/tasks/tests/test_vitaltasktemplate.py
@@ -162,3 +162,29 @@ class TestVitalTaskTemplateUsingPatient(TasksMixin, APITestCase):
     def test_delete_vital_task_template(self):
         response = self.client.delete(self.detail_url, {})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class TestVitalTaskTemplateSearch(TasksMixin, APITestCase):
+    def setUp(self):
+        self.fake = Faker()
+        self.patient = self.create_patient()
+        self.employee = self.create_employee()
+        self.user = self.employee.user
+        base_url = reverse('vital_task_templates-search-list')
+        self.search_url = f'{base_url}?q=sample'
+        self.client.force_authenticate(user=self.user)
+
+    def test_search_provider_role(self):
+        response = self.client.get(self.search_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_search_provider_role_patient_unauthorized(self):
+        self.client.logout()
+        self.client.force_authenticate(user=self.patient.user)
+        response = self.client.get(self.search_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_search_provider_role_unauthorized(self):
+        self.client.logout()
+        response = self.client.get(self.search_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
