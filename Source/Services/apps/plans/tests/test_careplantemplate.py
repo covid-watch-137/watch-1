2f8b672c014a6557a1d374f7444a3a9c79f37541
Ronil Rufo <rrufo@mindfiretechnology.com>
2019-01-06 14:34:29 +0800

    CA-1125: Fixed bugs in care plan template tests

--- Source/Services/apps/plans/tests/test_careplantemplate.py
+++ Source/Services/apps/plans/tests/test_careplantemplate.py
@@ -55,9 +55,11 @@ class TestCarePlanTemplateUsingEmployee(TasksMixin, APITestCase):
 
     def test_create_care_plan_template(self):
         template_type = self.create_care_plan_template_type()
+        service_area = self.create_service_area()
         payload = {
             'name': self.fake.name(),
             'type': template_type.id,
+            'service_area': service_area.id,
             'duration_weeks': random.randint(1, 5),
             'is_active': True
         }
@@ -66,9 +68,11 @@ class TestCarePlanTemplateUsingEmployee(TasksMixin, APITestCase):
 
     def test_full_update_care_plan_template(self):
         template_type = self.create_care_plan_template_type()
+        service_area = self.create_service_area()
         payload = {
             'name': self.fake.name(),
             'type': template_type.id,
+            'service_area': service_area.id,
             'duration_weeks': random.randint(1, 5),
             'is_active': True
         }
@@ -332,9 +336,11 @@ class TestCarePlanTemplateUsingPatient(TasksMixin, APITestCase):
 
     def test_create_care_plan_template(self):
         template_type = self.create_care_plan_template_type()
+        service_area = self.create_service_area()
         payload = {
             'name': self.fake.name(),
             'type': template_type.id,
+            'service_area': service_area.id,
             'duration_weeks': random.randint(1, 5),
             'is_active': True
         }
@@ -343,8 +349,10 @@ class TestCarePlanTemplateUsingPatient(TasksMixin, APITestCase):
 
     def test_full_update_care_plan_template(self):
         template_type = self.create_care_plan_template_type()
+        service_area = self.create_service_area()
         payload = {
             'name': self.fake.name(),
+            'service_area': service_area.id,
             'type': template_type.id,
             'duration_weeks': random.randint(1, 5),
             'is_active': True
