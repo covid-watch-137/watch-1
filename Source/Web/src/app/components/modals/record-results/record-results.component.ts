0df7eb62026feab7f744e7d2ad618171c88552f1
Jordan Price <jprice@izeni.com>
2019-01-22 04:53:19 -0700

    Finished wiring up history page with mock data

--- Source/Web/src/app/components/modals/record-results/record-results.component.ts
+++ Source/Web/src/app/components/modals/record-results/record-results.component.ts
@@ -9,15 +9,34 @@ export class RecordResultsComponent implements OnInit {
 
   public data = null;
 
+  public patient = null;
+  public date = null;
+  public carePlan = null;
+  public task = null;
+  public totalMinutes = null;
+  public with = null;
+  public syncToEHR = false;
+  public notes = '';
+  public patientEngagement = false;
+
   public tooltipRRM0Open;
   public tooltipRRM1Open;
   public tooltipRRM2Open;
 
-  constructor() {
-
-  }
+  constructor() { }
 
   public ngOnInit() {
     console.log(this.data);
+    if (this.data) {
+      this.patient = this.data.patient;
+      this.date = this.data.date;
+      this.carePlan = this.data.carePlan;
+      this.task = this.data.task;
+      this.totalMinutes = this.data.totalMinutes;
+      this.with = this.data.with;
+      this.syncToEHR = this.data.syncToEHR;
+      this.notes = this.data.notes;
+      this.patientEngagement = this.data.patientEngagement;
+    }
   }
 }
