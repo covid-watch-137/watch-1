1585fb4ca54bcfa647543c64b818ed81d1d361b6
Ronil Rufo <rrufo@mindfiretechnology.com>
2019-01-23 10:29:19 +0800

    CA-1189: Removed previous implementation of careplan by goals endpoint

--- Source/Services/apps/plans/api/urls.py
+++ Source/Services/apps/plans/api/urls.py
@@ -36,9 +36,4 @@ urlpatterns = [
         CarePlanTemplateByServiceArea.as_view(),
         name='service-area-plan-templates'
     ),
-    url(
-        r'^patient_profiles/(?P<parent_lookup_plan__patient>[0-9a-f-]+)/care_plan_templates/(?P<pk>[0-9a-f-]+)/goals/$',
-        GoalsByCarePlanTemplate.as_view(),
-        name='goal-by-plan-templates'
-    ),
 ]
