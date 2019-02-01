6c0e0793891245f39a616f4f7fd2bd3c6cdf303e
Ronil Rufo <rrufo@mindfiretechnology.com>
2019-02-01 15:35:26 +0800

    CA-1052: Implemented endpoint for billing overview

--- Source/Services/apps/billings/api/views.py
+++ Source/Services/apps/billings/api/views.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.db.models import Sum
 from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import viewsets, permissions, mixins
@@ -13,6 +14,7 @@ from .serializers import BilledActivitySerializer
 from apps.core.api.mixins import ParentViewSetPermissionMixin
 from apps.core.api.views import OrganizationViewSet
 from apps.core.models import Organization
+from apps.patients.models import PatientProfile
 
 
 class BilledActivityViewSet(viewsets.ModelViewSet):
@@ -153,3 +155,56 @@ class OrganizationBilledActivity(ParentViewSetPermissionMixin,
             )
 
         return queryset
+
+    def _get_billable_patients(self, queryset):
+        parents_query_dict = self.get_parents_query_dict()
+        organization_id = parents_query_dict['plan__patient__facility__organization']
+
+        # TODO: Filter this so only billable patients remain
+        return PatientProfile.objects.filter(
+            facility__organization__id=organization_id)
+
+    def get_billable_patients_count(self, queryset):
+        patients = self._get_billable_patients(queryset)
+        return patients.count()
+
+    def get_total_facilities(self, queryset):
+        patients = self._get_billable_patients(queryset)
+        return patients.values_list('facility', flat=True).distinct().count()
+
+    @action(methods=['get'], detail=False)
+    def overview(self, request, *args, **kwargs):
+        """
+        This endpoint will return aggregated data for an organization's billings.
+        Aggregated data are as follows:
+
+            - billable patients
+            - total facilities
+            - total practitioners
+            - total hours
+            - total billable
+
+        Page Usage
+        ---
+        - this endpoint will be used in `billing` page
+
+        """
+        base_queryset = self.get_queryset()
+        queryset = self.filter_queryset(base_queryset)
+
+        # TODO: Add this when the model is ready
+        total_practitioners = 0
+        total_billable = 0
+
+        time_spent = queryset.aggregate(total=Sum('time_spent'))
+        total_time_spent = time_spent['total'] or 0
+        total_hours = str(datetime.timedelta(minutes=total_time_spent))[:-3]
+
+        data = {
+            'billable_patients': self.get_billable_patients_count(queryset),
+            'total_facilities': self.get_total_facilities(queryset),
+            'total_practitioners': total_practitioners,
+            'total_hours': total_hours,
+            'total_billable': total_billable
+        }
+        return Response(data=data)
