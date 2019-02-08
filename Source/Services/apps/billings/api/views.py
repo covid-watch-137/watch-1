f7b01865f46b8d6ddf46d8bb31da8db2bc730e5d
kentw832 <kentwilson832@gmail.com>
2019-02-08 03:45:30 -0800

    complete CA-1163

--- Source/Services/apps/billings/api/views.py
+++ Source/Services/apps/billings/api/views.py
@@ -1,4 +1,10 @@
+import datetime
+
+from django.utils.translation import ugettext_lazy as _
+
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from ..models import BilledActivity
 from ..permissions import IsAdminOrEmployeeActivityOwner
@@ -62,3 +68,35 @@ class BilledActivityViewSet(viewsets.ModelViewSet):
             )
 
         return queryset
+
+    @action(methods=['post'], detail=False,
+            permission_classes=(permissions.IsAuthenticated, ))
+    def track_time(self, request, *args, **kwargs):
+        serializer = BilledActivitySerializer(
+            data=request.data,
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        pre_activity = BilledActivity.objects.filter(plan_id=serializer.validated_data.get('plan'),
+                                                     activity_type=serializer.validated_data.get('activity_type'),
+                                                     added_by_id=serializer.validated_data.get('added_by'),
+                                                     activity_date=datetime.date.today()) \
+                                             .first()
+        if pre_activity:
+            pre_activity.time_spent = pre_activity.time_spent + serializer.validated_data.get('time_spent')
+            pre_activity.save()
+        else:
+            serializer.save()
+
+        return Response(
+            {"detail": _("Successfully created a billed activity for the plan.")}
+        )
+
+    @action(methods=['get'], detail=False,
+            permission_classes=(permissions.IsAuthenticated, ))
+    def total_tracked_time(self, request, *args, **kwargs):
+        user = request.user
+        spent_time = BilledActivity().total_spent_time(user.employee_profile) if user.is_employee else 0
+        return Response(
+            {"total_tracked_time": spent_time}
+        )
