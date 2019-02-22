060ce7194550488d5305e0747be62af43fcf2f4d
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2019-02-22 13:03:54 +0800

    CA-1228: Added tests for emergency contact endpoints using employee as loggd in user

--- Source/Services/apps/patients/tests/test_emergencycontact.py
+++ Source/Services/apps/patients/tests/test_emergencycontact.py
@@ -21,9 +21,9 @@ from apps.tasks.utils import (
 )
 
 
-class TestEmergencyContact(PatientsMixin, APITestCase):
+class TestEmergencyContactUsingPatient(PatientsMixin, APITestCase):
     """
-    Test cases for :model:`tasks.PatientProfile` using a patient
+    Test cases for :model:`patients.EmergencyContact` using a patient
     as the logged in user.
     """
 
@@ -151,6 +151,151 @@ class TestEmergencyContact(PatientsMixin, APITestCase):
         patient = self.create_patient(**{
             'facility': self.facility
         })
+        emergency_contact = self.create_emergency_contact(**{
+            'patient': patient
+        })
+        url = reverse(
+            'patient-emergency-contacts-detail',
+            kwargs={
+                'parent_lookup_patient': patient.id,
+                'pk': emergency_contact.id
+            }
+        )
+        response = self.client.patch(url, payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_emergency_contact(self):
+        response = self.client.delete(self.detail_url, {})
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class TestEmergencyContactUsingEmployee(PatientsMixin, APITestCase):
+    """
+    Test cases for :model:`patients.EmergencyContact` using an employee
+    as the logged in user.
+    """
+
+    def setUp(self):
+        self.fake = Faker()
+        self.facility = self.create_facility()
+        self.employee = self.create_employee(**{
+            'facilities_managed': [self.facility]
+        })
+        self.patient = self.create_patient(**{
+            'facility': self.facility
+        })
+        self.user = self.employee.user
+        self.emergency_contact = self.create_emergency_contact(**{
+            'patient': self.patient
+        })
+
+        self.url = reverse(
+            'patient-emergency-contacts-list',
+            kwargs={
+                'parent_lookup_patient': self.patient.id
+            }
+        )
+        self.detail_url = reverse(
+            'patient-emergency-contacts-detail',
+            kwargs={
+                'parent_lookup_patient': self.patient.id,
+                'pk': self.emergency_contact.id
+            }
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_emergency_contacts_list(self):
+
+        # create dummy records
+        self.create_emergency_contact()
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.data['count'], 1)
+
+    def test_get_emergency_contact_detail(self):
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_emergency_contact_detail_unauthenticated(self):
+        self.client.logout()
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_emergency_contact_detail_not_owner(self):
+        patient = self.create_patient()
+        emergency_contact = self.create_emergency_contact(**{
+            'patient': patient
+        })
+        url = reverse(
+            'patient-emergency-contacts-detail',
+            kwargs={
+                'parent_lookup_patient': patient.id,
+                'pk': emergency_contact.id
+            }
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_emergency_contact(self):
+        payload = {
+            'first_name': self.fake.first_name(),
+            'last_name': self.fake.last_name(),
+            'relationship': self.fake.name(),
+            'phone': '1234567890',
+            'email': self.fake.email(),
+            'is_primary': True
+        }
+        response = self.client.post(self.url, payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_full_update_emergency_contact(self):
+        payload = {
+            'first_name': self.fake.first_name(),
+            'last_name': self.fake.last_name(),
+            'relationship': self.fake.name(),
+            'phone': '1234567890',
+            'email': self.fake.email(),
+            'is_primary': False
+        }
+        response = self.client.put(self.detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_full_update_emergency_contact_not_owner(self):
+        payload = {
+            'first_name': self.fake.first_name(),
+            'last_name': self.fake.last_name(),
+            'relationship': self.fake.name(),
+            'phone': '1234567890',
+            'email': self.fake.email(),
+            'is_primary': True
+        }
+
+        patient = self.create_patient()
+        emergency_contact = self.create_emergency_contact(**{
+            'patient': patient
+        })
+        url = reverse(
+            'patient-emergency-contacts-detail',
+            kwargs={
+                'parent_lookup_patient': patient.id,
+                'pk': emergency_contact.id
+            }
+        )
+        response = self.client.put(url, payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_partial_update_emergency_contact(self):
+        payload = {
+            'is_primary': False,
+        }
+        response = self.client.patch(self.detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_partial_update_emergency_contact_not_owner(self):
+        payload = {
+            'is_primary': False,
+        }
+        patient = self.create_patient()
         emergency_contact = self.create_emergency_contact(**{
             'patient': patient
         })
