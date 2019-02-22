29de2d835eb67614b2b862ac40469cb7a0d5a0c5
Ronil Rufo <rrufo@mindfiretechnology.com>
2019-02-22 17:29:41 +0800

    CA-1240: Fixed permission bugs and added tests for time spent field in care team members endpoint

--- Source/Services/apps/plans/tests/test_careteammember.py
+++ Source/Services/apps/plans/tests/test_careteammember.py
@@ -1,13 +1,18 @@
+import datetime
+
 from django.urls import reverse
+from django.utils import timezone
+
 from faker import Faker
+from dateutil.relativedelta import relativedelta
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .mixins import PlansMixin
 from apps.accounts.tests.factories import AdminUserFactory
+from apps.billings.tests.mixins import BillingsMixin
 
 
-class TestCareTeamMemberUsingAdmin(PlansMixin, APITestCase):
+class TestCareTeamMemberUsingAdmin(BillingsMixin, APITestCase):
     """
     Test cases for :model:`plans.CareTeamMember` using an admin
     as the logged in user.
@@ -24,18 +29,44 @@ class TestCareTeamMemberUsingAdmin(PlansMixin, APITestCase):
             'plan': self.plan
         })
 
-        self.delete_url = reverse(
+        self.detail_url = reverse(
             'care_team_members-detail',
             kwargs={'pk': self.team_member.id}
         )
         self.client.force_authenticate(user=self.user)
 
     def test_delete_team_member(self):
-        response = self.client.delete(self.delete_url, {})
+        response = self.client.delete(self.detail_url, {})
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
+    def test_get_care_team_member_detail_time_spent(self):
+        now = timezone.now()
+        last_month = now - relativedelta(months=1)
+        total_this_month = 0
+        for i in range(5):
+            activity = self.create_billed_activity(**{
+                'plan': self.plan,
+                'added_by': self.employee_member,
+                'activity_date': now
+            })
+            total_this_month += activity.time_spent
 
-class TestCareTeamMemberUsingEmployee(PlansMixin, APITestCase):
+        # create activities last month
+        for i in range(5):
+            self.create_billed_activity(**{
+                'plan': self.plan,
+                'added_by': self.employee_member,
+                'activity_date': last_month
+            })
+
+        response = self.client.get(self.detail_url)
+        self.assertEqual(
+            response.data['time_spent_this_month'],
+            str(datetime.timedelta(minutes=total_this_month))[:-3]
+        )
+
+
+class TestCareTeamMemberUsingEmployee(BillingsMixin, APITestCase):
     """
     Test cases for :model:`plans.CareTeamMember` using an employee
     as the logged in user.
@@ -59,14 +90,14 @@ class TestCareTeamMemberUsingEmployee(PlansMixin, APITestCase):
             'plan': self.plan
         })
 
-        self.delete_url = reverse(
+        self.detail_url = reverse(
             'care_team_members-detail',
             kwargs={'pk': self.team_member.id}
         )
         self.client.force_authenticate(user=self.user)
 
     def test_delete_team_member(self):
-        response = self.client.delete(self.delete_url, {})
+        response = self.client.delete(self.detail_url, {})
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_delete_team_member_not_manager(self):
@@ -77,18 +108,44 @@ class TestCareTeamMemberUsingEmployee(PlansMixin, APITestCase):
             'employee_profile': employee,
             'plan': self.plan
         })
-        response = self.client.delete(self.delete_url, {})
+        response = self.client.delete(self.detail_url, {})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_delete_team_member_not_member(self):
         self.client.logout()
         employee = self.create_employee()
         self.client.force_authenticate(user=employee.user)
-        response = self.client.delete(self.delete_url, {})
+        response = self.client.delete(self.detail_url, {})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+    def test_get_care_team_member_detail_time_spent(self):
+        now = timezone.now()
+        last_month = now - relativedelta(months=1)
+        total_this_month = 0
+        for i in range(5):
+            activity = self.create_billed_activity(**{
+                'plan': self.plan,
+                'added_by': self.employee_member,
+                'activity_date': now
+            })
+            total_this_month += activity.time_spent
 
-class TestCareTeamMemberUsingPatient(PlansMixin, APITestCase):
+        # create activities last month
+        for i in range(5):
+            self.create_billed_activity(**{
+                'plan': self.plan,
+                'added_by': self.employee_member,
+                'activity_date': last_month
+            })
+
+        response = self.client.get(self.detail_url)
+        self.assertEqual(
+            response.data['time_spent_this_month'],
+            str(datetime.timedelta(minutes=total_this_month))[:-3]
+        )
+
+
+class TestCareTeamMemberUsingPatient(BillingsMixin, APITestCase):
     """
     Test cases for :model:`plans.CareTeamMember` using a patient
     as the logged in user.
@@ -106,12 +163,35 @@ class TestCareTeamMemberUsingPatient(PlansMixin, APITestCase):
             'plan': self.plan
         })
 
-        self.delete_url = reverse(
+        self.detail_url = reverse(
             'care_team_members-detail',
             kwargs={'pk': self.team_member.id}
         )
         self.client.force_authenticate(user=self.user)
 
     def test_delete_team_member(self):
-        response = self.client.delete(self.delete_url, {})
+        response = self.client.delete(self.detail_url, {})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_care_team_member_detail_time_spent(self):
+        now = timezone.now()
+        last_month = now - relativedelta(months=1)
+        total_this_month = 0
+        for i in range(5):
+            activity = self.create_billed_activity(**{
+                'plan': self.plan,
+                'added_by': self.employee_member,
+                'activity_date': now
+            })
+            total_this_month += activity.time_spent
+
+        # create activities last month
+        for i in range(5):
+            self.create_billed_activity(**{
+                'plan': self.plan,
+                'added_by': self.employee_member,
+                'activity_date': last_month
+            })
+
+        response = self.client.get(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
