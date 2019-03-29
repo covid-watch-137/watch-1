ca89d290b147c143d143fe5159d1ac4b3eaca83a
Nathan Lisch <nathan.lisch@gmail.com>
2019-03-29 16:23:09 -0600

    fixed password change and emergency contact

--- Source/Web/src/app/routes/users/detail/modals/change-password/change-password.component.ts
+++ Source/Web/src/app/routes/users/detail/modals/change-password/change-password.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../../../../../modules/modals';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {AppConfig} from '../../../../../app.config';
 
 @Component({
   selector: 'app-change-password',
@@ -10,15 +12,42 @@ export class ChangePasswordComponent implements OnInit {
 
   public data = null;
 
+  public oldPassword;
+  public newPassword1;
+  public newPassword2;
+
   public tooltipCPM0Open;
 
   constructor(
     public modals: ModalService,
+    public http: HttpClient,
   ) {
 
   }
 
   public ngOnInit() {
     console.log(this.data);
+  }
+
+  public get submitDisabled() {
+    return !(this.oldPassword && this.newPassword1 && this.newPassword2);
+  }
+
+  public save() {
+    this.http.request('POST', `${AppConfig.baseUrl}/rest-auth/password/change/`, {
+      body: {
+        old_password: this.oldPassword,
+        new_password1: this.newPassword1,
+        new_password2: this.newPassword2,
+      },
+      headers: new HttpHeaders().set('Accept', 'application/json'),
+    }).subscribe((res) => {
+      this.close();
+    })
+  }
+
+
+  public close(data = null) {
+    this.modals.close(data);
   }
 }
