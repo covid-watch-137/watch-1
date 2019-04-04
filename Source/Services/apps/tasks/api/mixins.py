e22fb2dcffbc4e88b5f79c972d2fc26ac42d0d0b
Ronil Rufo <rrufo@mindfiretechnology.com>
2019-04-04 15:19:41 +0800

    CA-1284: Added documentation for DestroyTemplateMixin

--- Source/Services/apps/tasks/api/mixins.py
+++ Source/Services/apps/tasks/api/mixins.py
@@ -2,6 +2,13 @@ from django.utils import timezone
 
 
 class DestroyTemplateMixin(object):
+    """
+    Custom delete behavior for Task Template models.
+    This mixin will do the following:
+
+        - remove all task after the current date
+        - mark the template as inactive (`is_active=False`)
+    """
 
     def perform_destroy(self, instance):
         now = timezone.now()
