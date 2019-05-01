a7caa31f5b6db40f83d70fd2a423259466aabf59
Ronil Rufo <rrufo@mindfiretechnology.com>
2019-05-01 09:01:09 +0800

    CA-1166: Propagated CarePlanAssessmentTemplate changes into assessment response tests

--- Source/Services/apps/tasks/tests/factories.py
+++ Source/Services/apps/tasks/tests/factories.py
@@ -125,6 +125,7 @@ class AssessmentQuestionFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = 'tasks.AssessmentQuestion'
+        django_get_or_create = ('assessment_task_template', )
 
 
 class AssessmentResponseFactory(factory.django.DjangoModelFactory):
