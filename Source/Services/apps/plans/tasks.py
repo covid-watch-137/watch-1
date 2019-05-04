98ae12c30cf724e4a16e565d0986bd66c4b6521d
kentw832 <kentwilson832@gmail.com>
2019-05-04 04:58:34 -0700

    implement results_over_time for care plan

--- Source/Services/apps/plans/tasks.py
+++ Source/Services/apps/plans/tasks.py
@@ -1,8 +1,22 @@
+import datetime
+
+from django.db.models import Avg, Sum
+from datetime import timedelta
+from django.utils import timezone
+
 from celery.task import PeriodicTask
 from celery.schedules import crontab
 
 from .models import CarePlanResultOverTime
-from .serializers import CarePlanOverviewSerializer
+
+from apps.tasks.models import (
+    TeamTask,
+    AssessmentTask,
+    PatientTask,
+    MedicationTask,
+    SymptomTask,
+    VitalTask,
+)
 
 
 class DailyInfoMessage(PeriodicTask):
@@ -13,11 +27,73 @@ class DailyInfoMessage(PeriodicTask):
     patients' care plan templates.
     """
 
-    # Runs every week
+    # Runs every Monday
     run_every = crontab(minute='0', hour='0', day_of_week='mon')
+
+    def get_outcome(self, plan):
+        now = timezone.now()
+        date = now.date() - timedelta(days=7)
+        tasks = AssessmentTask.objects.filter(
+            plan=obj,
+            assessment_task_template__tracks_outcome=True,
+            due_datetime__range=[date, now]
+        ).aggregate(average=Avg('responses__rating'))
+        average = tasks['average'] or 0
+        return round((average / 5) * 100)
+
+    def get_engagement(self, plan):        
+        now = timezone.now()
+        date = now.date() - timedelta(days=7)
+        patient_tasks = PatientTask.objects.filter(
+            patient_template__plan=obj,
+            due_datetime__range=[date, now])
+        medication_tasks = MedicationTask.objects.filter(
+            medication_task_template__plan=obj,
+            due_datetime__range=[date, now])
+        symptom_tasks = SymptomTask.objects.filter(
+            symptom_template__plan=obj,
+            due_datetime__range=[date, now])
+        assessment_tasks = AssessmentTask.objects.filter(
+            plan=obj,
+            due_datetime__range=[date, now])
+        vital_tasks = VitalTask.objects.filter(
+            plan=obj,
+            due_datetime__range=[date, now])
+
+        total_patient_tasks = patient_tasks.count()
+        total_medication_tasks = medication_tasks.count()
+        total_symptom_tasks = symptom_tasks.count()
+        total_assessment_tasks = assessment_tasks.count()
+        total_vital_tasks = vital_tasks.count()
+
+        completed_patient_tasks = patient_tasks.filter(
+            status__in=['missed', 'done']).count()
+        completed_medication_tasks = medication_tasks.filter(
+            status__in=['missed', 'done']).count()
+        completed_symptom_tasks = symptom_tasks.filter(
+            is_complete=True).count()
+        completed_assessment_tasks = assessment_tasks.filter(
+            is_complete=True).count()
+        completed_vital_tasks = vital_tasks.filter(
+            is_complete=True).count()
+
+        total_completed = (completed_patient_tasks +
+                           completed_medication_tasks +
+                           completed_symptom_tasks +
+                           completed_assessment_tasks +
+                           completed_vital_tasks)
+        total_tasks = (total_patient_tasks +
+                       total_medication_tasks +
+                       total_symptom_tasks +
+                       total_assessment_tasks +
+                       total_vital_tasks)
+        return round((total_completed / total_tasks) * 100) \
+            if total_tasks > 0 else 0
 
     def run(self):
         for plan in CarePlan.objects.all():
-            CarePlanOverviewSerializer(plan).data
-            CarePlanResultOverTime.objects.create()
-
+            CarePlanResultOverTime.objects.create(
+                plan=plan,
+                outcome=self.get_outcome(plan),
+                engagement=self.get_engagement(plan)
+            )
