90818416ca60c5371c144042ec95c182c5be9cc5
Ronil Rufo <rrufo@mindfiretechnology.com>
2019-05-07 16:34:42 +0800

    CA-1166: Added validation for custom fields in CarePlanSymptomTemplateSerializer

--- Source/Services/apps/tasks/api/serializers.py
+++ Source/Services/apps/tasks/api/serializers.py
@@ -519,6 +519,46 @@ class CarePlanSymptomTemplateSerializer(RepresentationMixin,
             }
         ]
 
+    def validate(self, data):
+        symptom_task_template = data.get('symptom_task_template')
+
+        if self.instance is not None and symptom_task_template is None:
+            symptom_task_template = self.instance.symptom_task_template
+
+        if symptom_task_template is None:
+            custom_start_on_day = data.get('custom_start_on_day')
+            custom_frequency = data.get('custom_frequency')
+            custom_repeat_amount = data.get('custom_repeat_amount')
+            custom_appear_time = data.get('custom_appear_time')
+            custom_due_time = data.get('custom_due_time')
+
+            if custom_start_on_day is None:
+                raise serializers.ValidationError({
+                    'custom_start_on_day': _('This field is required.')
+                })
+
+            if custom_frequency is None:
+                raise serializers.ValidationError({
+                    'custom_frequency': _('This field is required.')
+                })
+
+            if custom_repeat_amount is None:
+                raise serializers.ValidationError({
+                    'custom_repeat_amount': _('This field is required.')
+                })
+
+            if custom_appear_time is None:
+                raise serializers.ValidationError({
+                    'custom_appear_time': _('This field is required.')
+                })
+
+            if custom_due_time is None:
+                raise serializers.ValidationError({
+                    'custom_due_time': _('This field is required.')
+                })
+
+        return data
+
 
 class SymptomRatingSerializer(RepresentationMixin,
                               serializers.ModelSerializer):
