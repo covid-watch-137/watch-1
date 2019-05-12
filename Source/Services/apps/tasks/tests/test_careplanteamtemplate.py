a9583382ae1b05d6b334cc11d993dbe78d5d47d1
Ronil Rufo <rrufo@mindfiretechnology.com>
2019-05-12 18:22:11 +0800

    CA-1166: Added tests for updating team_task_template and custom fields in CarePlanTeamTemplate

--- Source/Services/apps/tasks/tests/test_careplanteamtemplate.py
+++ Source/Services/apps/tasks/tests/test_careplanteamtemplate.py
@@ -198,6 +198,113 @@ class TestCarePlanTeamTemplateUsingEmployee(TasksMixin, APITestCase):
         response = self.client.patch(self.detail_url, payload)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_partial_update_team_template_remove_template(self):
+        plan = self.create_care_plan(
+            self.patient,
+            plan_template=self.plan_template
+        )
+        team_task_template = self.create_team_task_template(
+            plan_template=self.plan_template
+        )
+        team_template = self.create_plan_team_template(
+            plan=plan,
+            team_task_template=team_task_template
+        )
+
+        payload = {
+            'team_task_template': '',
+        }
+        detail_url = reverse(
+            'plan_team_templates-detail',
+            kwargs={
+                'pk': team_template.id
+            }
+        )
+        response = self.client.patch(detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_partial_update_team_template_add_template(self):
+        plan = self.create_care_plan(
+            self.patient,
+            plan_template=self.plan_template
+        )
+        team_task_template = self.create_team_task_template(
+            plan_template=self.plan_template
+        )
+        team_template = self.create_plan_team_template(
+            plan=plan,
+            team_task_template=None
+        )
+
+        payload = {
+            'team_task_template': team_task_template.id,
+        }
+        detail_url = reverse(
+            'plan_team_templates-detail',
+            kwargs={
+                'pk': team_template.id
+            }
+        )
+        response = self.client.patch(detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_partial_update_team_template_without_template(self):
+        plan = self.create_care_plan(
+            self.patient,
+            plan_template=self.plan_template
+        )
+
+        team_template = self.create_plan_team_template(
+            plan=plan,
+            team_task_template=None,
+            custom_start_on_day=random.randint(1, 5),
+            custom_frequency='once',
+            custom_repeat_amount=-1,
+            custom_appear_time=datetime.time(8, 0, 0),
+            custom_due_time=datetime.time(17, 0, 0)
+        )
+
+        payload = {
+            'custom_start_on_day': random.randint(1, 5),
+        }
+        detail_url = reverse(
+            'plan_team_templates-detail',
+            kwargs={
+                'pk': team_template.id
+            }
+        )
+        response = self.client.patch(detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_partial_update_team_template_without_template_remove(self):
+        plan = self.create_care_plan(
+            self.patient,
+            plan_template=self.plan_template
+        )
+
+        team_template = self.create_plan_team_template(
+            plan=plan,
+            team_task_template=None,
+            custom_start_on_day=random.randint(1, 5),
+            custom_frequency='once',
+            custom_repeat_amount=-1,
+            custom_appear_time=datetime.time(8, 0, 0),
+            custom_due_time=datetime.time(17, 0, 0)
+        )
+
+        payload = {
+            'custom_start_on_day': '',
+        }
+        detail_url = reverse(
+            'plan_team_templates-detail',
+            kwargs={
+                'pk': team_template.id
+            }
+        )
+        response = self.client.patch(detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue('custom_start_on_day' in response.data)
+
     def test_partial_update_team_template_not_member(self):
         payload = {
             'custom_start_on_day': random.randint(1, 5),
