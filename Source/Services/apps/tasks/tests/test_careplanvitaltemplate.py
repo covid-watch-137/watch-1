50b58976b2d549f98d43b78cfad8329a041ab812
Ronil Rufo <rrufo@mindfiretechnology.com>
2019-05-12 21:53:32 +0800

    CA-1166: Added tests for updating and serializing instructions in CarePlanVitalTemplate

--- Source/Services/apps/tasks/tests/test_careplanvitaltemplate.py
+++ Source/Services/apps/tasks/tests/test_careplanvitaltemplate.py
@@ -325,6 +325,38 @@ class TestCarePlanVitalTemplateUsingEmployee(TasksMixin, APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertTrue('custom_start_on_day' in response.data)
 
+    def test_partial_update_vital_template_instructions(self):
+        plan = self.create_care_plan(
+            self.patient,
+            plan_template=self.plan_template
+        )
+        instructions = self.fake.sentence(nb_words=10)
+
+        vital_template = self.create_plan_vital_template(
+            plan=plan,
+            vital_task_template=None,
+            custom_name=self.fake.name(),
+            custom_start_on_day=random.randint(1, 5),
+            custom_frequency='once',
+            custom_repeat_amount=-1,
+            custom_appear_time=datetime.time(8, 0, 0),
+            custom_due_time=datetime.time(17, 0, 0),
+        )
+
+        payload = {
+            'custom_start_on_day': random.randint(1, 5),
+            'custom_instructions': instructions
+        }
+        detail_url = reverse(
+            'plan_vital_templates-detail',
+            kwargs={
+                'pk': vital_template.id
+            }
+        )
+        response = self.client.patch(detail_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['instructions'], instructions)
+
     def test_partial_update_vital_template_not_member(self):
         payload = {
             'custom_start_on_day': random.randint(1, 5),
