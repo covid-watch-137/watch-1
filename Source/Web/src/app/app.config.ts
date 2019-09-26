30050d2ff099fde7bfb2c62a2838a80a7696404e
bcarbone <bcarbone@mindfiretechnology.com>
2019-09-26 06:23:26 -0600

    Added a null check before setting the loglevel

--- Source/Web/src/app/app.config.ts
+++ Source/Web/src/app/app.config.ts
@@ -1,6 +1,13 @@
 import { environment } from '../environments/environment';
+import { Utils } from './utils';
 
 export class AppConfig {
+
+  constructor() {
+    if (!Utils.isNullOrUndefined(environment.minimumLoggingLevel)) {
+      Utils.minimumLoggingLevel = environment.minimumLoggingLevel;
+    }
+  }
 
   public static baseUrl = environment.apiHost;
   public static apiUrl = AppConfig.createUrl('api');
